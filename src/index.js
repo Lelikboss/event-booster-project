235473dff01e1d378ce8b1b27062fcfbb696102f
Alexey Alexeew <lelikboss97@gmail.com>
2021-09-16 16:23:40 +0300

    add file with API/ add axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+import getEventApi from './js/apiServices.js';
+let countryCode = ' ';
+let page = 1;
+let amountEl = 20;
+let keyword = ' ';
+getEventApi({ countryCode, page, amountEl, keyword }).then(result => console.log(result.data));
