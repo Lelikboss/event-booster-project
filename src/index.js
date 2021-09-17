3d119269fcb041e6bfece8f6e66327d60e9b014b
Alexey Alexeew <lelikboss97@gmail.com>
2021-09-17 15:44:46 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import './sass/main.scss';
+import { itemEventMarcup } from './js/marcup.js';
 import getEventApi from './js/apiServices.js';
-let countryCode = ' ';
+let countryCode = 'US';
 let page = 1;
-let amountEl = 20;
 let keyword = ' ';
-getEventApi({ countryCode, page, amountEl, keyword }).then(result => console.log(result.data));
+let amountEl = 20;
+
+const amountElChange = () => {
+  if (window.matchMedia('(min-width: 768px) and (max-width: 1279.98px)').matches) {
+    amountEl = 21;
+  } else {
+    amountEl = 20;
+  }
+};
+
+const createEventMarcup = () => {
+  amountElChange();
+  getEventApi({ countryCode, page, amountEl, keyword })
+    .then(result => {
+      itemEventMarcup(result.data._embedded.events);
+      console.log(result.data._embedded.events);
+    })
+    .catch(err => console.log(err));
+};
+createEventMarcup();
--- src/js/apiServices.js
+++ src/js/apiServices.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 const BASIC_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?';
 const KEY = '5YjHLxiERacoASpI4A0M5bRuOeVRN8nS';
+
 const getEventApi = async ({ countryCode, page, amountEl, keyword }) => {
   try {
     const res = await axios.get(
-      `${BASIC_URL}classificationName=music&countryCode=${countryCode}&keyword=${keyword}&page=${page}&size=${amountEl}&apikey=${KEY}`,
+      `${BASIC_URL}&countryCode=${countryCode}&keyword=${keyword}&page=${page}&size=${amountEl}&apikey=${KEY}`,
     );
     return res;
   } catch (error) {
