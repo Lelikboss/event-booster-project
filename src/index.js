07ab4371ed1140001f96d6c517217210070ae112
Alexey Alexeew <lelikboss97@gmail.com>
2021-09-18 12:29:47 +0300

    add scroll; add search countries

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import './sass/main.scss';
 import { itemEventMarcup } from './js/marcup.js';
 import getEventApi from './js/apiServices.js';
-let countryCode = 'US';
+import { getAllCountriesNames } from 'get-all-country-info';
+import { getCode } from 'country-list';
+import { debounce } from 'lodash';
+let countryCode = ' ';
 let page = 1;
 let keyword = ' ';
 let amountEl = 20;
+import SimpleBar from 'simplebar';
+import 'simplebar/dist/simplebar.css';
+import refs from './js/refs';
+new SimpleBar(document.getElementById('countries'), { autoHide: false });
+const listCountryMarcup = getAllCountriesNames()
+  .map(e => `<option>${e}</option>`)
+  .sort()
+  .join(' ');
+const checkCountry = e => {
+  e.preventDefault();
+  console.log(e.target.value);
+  let countryName = e.target.value;
+  refs.eventsContainer.innerHTML = '';
+  countryCode = getCode(countryName);
+  console.log(countryCode);
+  createEventMarcup();
+};
+refs.dataCountryList.addEventListener('click', debounce(checkCountry, 1000));
+refs.dataCountryList.insertAdjacentHTML('beforeend', listCountryMarcup);
 
+refs.simpleEl.style.position = 'absolute';
 const amountElChange = () => {
   if (window.matchMedia('(min-width: 768px) and (max-width: 1279.98px)').matches) {
     amountEl = 21;
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,8 @@
 const refs = {
   eventMarcup: document.querySelector('.js-events-list'),
+  inputCountryEl: document.querySelector('.header__input-js'),
+  dataCountryList: document.querySelector('#countries'),
+  simpleEl: document.querySelector('[data-simplebar]'),
+  eventsContainer: document.querySelector('.js-events-list'),
 };
 export default refs;
