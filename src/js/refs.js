5fd2ea4eb05f79fed6388bb8e846e910813c6500
Karina Klevetska <karina.klevetska@gmail.com>
2021-09-19 22:32:22 +0300

    feature/input-logic-style-pnotify

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,7 +3,8 @@ const refs = {
   inputCountryEl: document.querySelector('.header__input-js'),
   dataCountryList: document.querySelector('#countries'),
   simpleEl: document.querySelector('[data-simplebar]'),
+  datalist: document.querySelector('datalist'),
   eventsContainer: document.querySelector('.js-events-list'),
-  inputEventSearch: document.querySelector('.header__search-form'),
+  inputEventSearch: document.querySelector('.header__input-search'),
 };
 export default refs;
