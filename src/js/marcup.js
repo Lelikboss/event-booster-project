be27a5a22eb17bbba48fd5a8b481eb2934996de9
Semivetal <leninec152@gmail.com>
2021-09-20 15:29:11 +0300

    modal_commit_2

--- src/js/marcup.js
+++ src/js/marcup.js
@@ -1,6 +1,7 @@
 import eventTemplates from '../templates/events.hbs';
 import refs from './refs.js';
 import { getAllCountriesNames } from 'get-all-country-info';
+import modalTemplates from '../templates/modal.hbs';
 
 export const itemEventMarcup = result => {
   const markup = eventTemplates(result);
@@ -10,3 +11,8 @@ export const listCountryMarcup = getAllCountriesNames()
   .map(e => `<option>${e}</option>`)
   .sort()
   .join(' ');
+
+export const modalEventMarcup = response => {
+    const modalWindow = modalTemplates(response);
+    refs.bodyEl.insertAdjacentHTML('beforeend', modalWindow);
+}
