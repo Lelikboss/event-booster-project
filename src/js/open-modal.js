5e496bcd4c7825173db6bdb95bb01547887903a9
Sulimovsky <bohdancsnstr@gmail.com>
2021-09-21 18:06:19 +0300

    Работает закрытые на все нужные события

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -7,14 +7,25 @@ refs.eventMarcup.addEventListener('click', openModal);
 
 function openModal(e) {
     if (e.target.dataset.id) {
-        console.log(e.target.dataset.id);
         const idNum = e.target.dataset.id;
     
         getIdApi(idNum).then(res => {
           const instance = basicLightbox.create(template(res), {
             onShow: instance => {
               instance.element().querySelector('[data-action="modal-close"]').onclick = () => instance.close();
-            }
+              window.addEventListener('keydown', (e) => {
+                if (e.code === 'Escape') {
+                  instance.close();
+                }
+              });
+            },
+            // onShow: instance => {  // странно так не работает
+            //   window.addEventListener('keydown', (e) => {
+            //     if (e.code === 'Escape') {
+            //       instance.close();
+            //     }
+            //   })
+            // }
           });
 
           instance.show();
