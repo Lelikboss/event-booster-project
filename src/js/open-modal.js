932646bda89cb6f2ff7b29988903164f4a8d3adf
Sulimovsky <bohdancsnstr@gmail.com>
2021-09-23 16:07:49 +0300

    При открытой модалке на боди оверфлоу

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -5,22 +5,22 @@ import refs from './refs';
 
 refs.eventMarcup.addEventListener('click', openModal);
 function openModal(e) {
-  // refs.bodyEl.classList.toggle('overflow--hidden');
   if (e.target.dataset.id) {
     const idNum = e.target.dataset.id;
+    refs.bodyEl.classList.add('overflow--hidden');
 
     getIdApi(idNum).then(res => {
       const instance = basicLightbox.create(template(res), {
         onShow: instance => {
           instance.element().querySelector('[data-action="modal-close"]').onclick = () => {
             instance.close();
-            // refs.bodyEl.classList.toggle('overflow--hidden');
+            refs.bodyEl.classList.remove('overflow--hidden');
           };
           window.addEventListener('keydown', e => {
             console.log(e.code);
             if (e.code === 'Escape') {
               instance.close();
-              // refs.bodyEl.classList.toggle('overflow--hidden');
+              refs.bodyEl.classList.remove('overflow--hidden');
             }
           });
         },
