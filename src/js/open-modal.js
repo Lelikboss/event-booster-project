9be780debcaeaba69c52bd45dca8603641cb36f4
Sulimovsky <bohdancsnstr@gmail.com>
2021-09-23 18:57:14 +0300

    Изминения в срипте модалки, убран ховер, изминения в шаблоне

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -1,7 +1,7 @@
 import * as basicLightbox from 'basiclightbox';
 import { getIdApi } from './apiServices';
 import template from '../templates/modal-events.hbs';
-import { throttle } from 'lodash';
+import { lowerFirst, throttle } from 'lodash';
 import refs from './refs';
 
 refs.eventMarcup.addEventListener('click', openModal);
@@ -11,8 +11,13 @@ function openModal(e) {
     refs.bodyEl.classList.add('overflow--hidden');
 
     getIdApi(idNum).then(res => {
+      console.log(res.data._embedded.events[0]._embedded.attractions);
+      console.log(res.data._embedded.events[0]._embedded.attractions[0].hasOwnProperty('externalLinks'));
+      console.log(res.data._embedded.events[0]._embedded.attractions.length >= 2);
+
       const instance = basicLightbox.create(template(res), {
-        onShow: instance => {
+        onShow: 
+          instance => {
           instance.element().querySelector('[data-action="modal-close"]').onclick = () => {
             instance.close();
             refs.bodyEl.classList.remove('overflow--hidden');
@@ -27,8 +32,26 @@ function openModal(e) {
             if (e.target.classList.contains('basicLightbox')) {
               refs.bodyEl.classList.remove('overflow--hidden');
             }
-          })
-        },
+          });
+          const isPropEvents = res.data._embedded.events[0].hasOwnProperty('priceRanges');
+          if (isPropEvents && res.data._embedded.events[0].priceRanges.length < 3) {
+            const hiddenEl = instance.element().querySelector('.centered--left--vip');
+            hiddenEl.classList.add('visually-hidden');
+          } 
+          else if (isPropEvents === false) {
+            const contentEl = instance.element().querySelector('#vip');
+            contentEl.classList.remove('modal__item-content--icon');
+            contentEl.textContent = 'The organizer did not put VIP tickets on sale.';
+          };
+
+          // const isPropAttractions = res.data._embedded.events[0]._embedded.attractions[0].hasOwnProperty('externalLinks');
+          // if (isPropAttractions && res.data._embedded.events[0]._embedded.attractions.length >= 2) {
+          //   const moreBtn = instance.element().querySelector('.modal__more-btn-container');
+          //   const string = `<a href="${res.data._embedded.events[0]._embedded.attractions[1].externalLinks.homepage[1].url}" target="_blank" rel="noopener noreferer"
+          //   class="modal__more-btn link">MORE FROM THIS ${res.data._embedded.events[0]._embedded.attractions[1].name}</a>`
+          //   moreBtn.insertAdjacentHTML('beforeend', string);
+          // }
+          },
       });
 
       instance.show();
